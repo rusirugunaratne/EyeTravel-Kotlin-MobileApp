48938ce4108baada8e40f5cea2e8575619b1b19f
rusirugunaratne <info.rusiru@gmail.com>
2023-05-01 01:42:47 +0530

    mc update

--- app/src/main/java/com/example/eyetravel/MainActivity.kt
+++ app/src/main/java/com/example/eyetravel/MainActivity.kt
@@ -2,50 +2,12 @@ package com.example.eyetravel
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.fragment.app.Fragment
-import com.example.eyetravel.databinding.ActivityMainBinding
-import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var binding : ActivityMainBinding
-
     override fun onCreate(savedInstanceState: Bundle?) {
-//        super.onCreate(savedInstanceState)
-//        supportActionBar?.hide();
-//        setContentView(R.layout.user_layout)
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-        replaceFragment(Home())
-
-        findViewById<BottomNavigationView>(R.id.) {
-
-            when(it.itemId){
-
-                R.id.home -> replaceFragment(Home())
-                R.id.profile -> replaceFragment(Profile())
-                R.id.settings -> replaceFragment(Settings())
-
-                else ->{
-
-
-
-                }
-
-            }
-
-            true
-
-        }
-
-
-    }
-
-    private fun replaceFragment (fragment: Fragment){
-        val fragmentManager = supportFragmentManager
-        val fragmentTransaction = fragmentManager.beginTransaction()
-        fragmentTransaction.replace(R.id.frameLayout, fragment)
-        fragmentTransaction.commit()
+        supportActionBar?.hide();
+        setContentView(R.layout.activity_main)
     }
 }
